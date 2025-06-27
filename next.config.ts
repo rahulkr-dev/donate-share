5d724bc19a20552831af80c4813aee0a0fc19d98
rahulkr-dev <rkrahul8181@gmail.com>
2025-06-27 14:16:04 +0530

    feat: update next.config.ts to include additional image remote patterns for enhanced image loading

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next"
 
+// https://source.unsplash.com/600x400/?art,supplies
+
+// ["https://burst.shopifycdn.com/photos/yellow-couch-by-black-and-white-mural_925x.jpg","https://burst.shopifycdn.com/photos/house-plant-in-white-pot_925x.jpg","https://burst.shopifycdn.com/photos/condominium-interior-livingroom_925x.jpg"]
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -8,6 +12,12 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "wappify.fly.storage.tigris.dev",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "burst.shopifycdn.com",
         port: "",
         pathname: "/**",
       },
